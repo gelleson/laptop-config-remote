89adf117a562054a0ca747c2d9f8809b0ff121f6
Altynbek Kaliakbarov <go.gelleson@gmail.com>
2024-10-13 18:24:14 +0500

    feat(settings): add Zed editor and Git message templates

--- modules/home.nix
+++ modules/home.nix
@@ -41,47 +41,6 @@
       };
     };
 
-
-    home.file.".config/zed/settings.json".text = ''
-    // Zed setting
-    //
-    // For information on how to configure Zed, see the Zed
-    // documentation: https://zed.dev/docs/configuring-zed
-    //
-    // To see all of Zed's default settings without changing your
-    // custom settings, run `zed: open default settings` from the
-    // command palette (cmd-shift-p / ctrl-shift-p)
-    {
-      "ui_font_size": 16,
-      "autosave": "on_focus_change",
-      "buffer_font_size": 18,
-      "base_keymap": "JetBrains",
-      "format_on_save": "on",
-      "git_status": true,
-      "theme": {
-        "mode": "system",
-        "light": "One Light",
-        "dark": "One Dark"
-      },
-      "buffer_font_family": "JetBrains Mono"
-    }
-    '';
-
-    home.file.".gitmessage".text = ''
-      # Commit message
-      # gelleson
-
-      # Why:
-
-      # This change is necessary because
-
-      # How:
-
-      # This change addresses the need by
-
-      # Refs:
-    '';
-
     programs.alacritty = {
         enable = true;
         settings = {
@@ -111,5 +70,6 @@
 
     imports = [
     ./files/templates.nix
+    ./files/settings.nix
     ];
 }
