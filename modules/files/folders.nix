1087911efbe440cba3bc61f73b84c08f101dd1aa
Altynbek Kaliakbarov <go.gelleson@gmail.com>
2024-10-13 18:40:47 +0500

    feat(folders): add README files for personal, OSS, and commercial projects

--- modules/files/folders.nix
+++ modules/files/folders.nix
@@ -1,19 +1,38 @@
 { config, pkgs, home, ... }:{
 
-    # Ensure the codes directory exists
-    home.file."codes" = {
-        source = null; # Use null to create an empty folder
-        mode = "0755"; # Set folder permissions
-    };
-
     # Create the personal directory within codes
-    home.file."codes/personal" = {
-        source = null; # Use null to create an empty folder
-        mode = "0755"; # Set folder permissions
-    };
+    home.file."codes/personal/README.md".text = ''
+    # Personal Projects Directory
 
-    home.file."codes/commercial" = {
-        source = null; # Use null to create an empty folder
-        mode = "0755"; # Set folder permissions
-    };
+    This directory contains personal coding projects created for self-learning and experimentation. The projects here are aimed at enhancing my programming skills and exploring new technologies.
+
+    ## Purpose
+
+    The personal projects include various coding exercises, small applications, and experiments. They serve as a playground for trying out new ideas, learning different programming languages, and improving coding practices.
+
+
+    Feel free to explore and learn from these projects!
+    '';
+
+    home.file."codes/personal/oss/README.md".text = ''
+    # Open Source Software Projects
+
+    This directory contains my contributions to open source software (OSS) projects. These projects reflect my commitment to the open-source community and my desire to collaborate with others on meaningful software development.
+
+    ## Purpose
+
+    The projects in this directory are designed to contribute to the open-source ecosystem. They may include standalone applications, libraries, or contributions to existing open-source projects. My goal is to enhance the usability, performance, and functionality of software through collaborative development..
+    '';
+
+    home.file."codes/commercial/README.md".text = ''
+    # Commercial Projects Directory
+
+    This directory contains coding projects developed for commercial purposes, including client work and freelance projects. These projects showcase my professional software development skills.
+
+    ## Purpose
+
+    The commercial projects are created to meet client requirements and deliver functional software solutions. They reflect my experience in working with various technologies and managing software development processes.
+
+    These projects illustrate my ability to deliver high-quality software solutions for clients.
+    '';
 }
