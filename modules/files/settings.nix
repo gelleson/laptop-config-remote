4e23088643b9081ce6e92d2d166a30dd977a6ed9
gelleson <go.gelleson@gmail.com>
2025-03-30 19:12:12 +0500

    feat(files): add Gemini 2.5 Pro model

--- modules/files/settings.nix
+++ modules/files/settings.nix
@@ -98,6 +98,11 @@ let
             display_name = "Qwen QvQ 32b";
             max_tokens = 131000;
           }
+          {
+            name = "openrouter/google/gemini-2.5-pro-exp-03-25:free";
+            display_name = "Gemini 2.5 Pro Exp 03-25";
+            max_tokens = 1000000;
+          }
         ];
         version = "1";
       };
